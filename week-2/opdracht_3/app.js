1ad81ad313b1c268abfb4c367d98ef80f0833aab
Pending345 <rafhildert@gmail.com>
2024-11-27 09:00:23 +0100

    opdracht_3

---
+++ week-2/opdracht_3/app.js
@@ -0,0 +1,15 @@
+//var lijst = [];
+
+//for (let i = 0; i <= 100; i++) {
+//    lijst.push(i);
+//}
+
+//document.getElementById("output").textContent = lijst;
+
+var lijst = [];
+
+for (let i = 100; i >= 0; i--) {
+    lijst.push(i);
+}
+
+document.getElementById("output").textContent = lijst;
